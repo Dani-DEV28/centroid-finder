b645a4de2d86987891606a3168ddc91c769fa3dc
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-14 11:07:59 -0700

    Set up DfsBinaryGroupFinderTest file

---
+++ src/DfsBinaryGroupFinderTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+public class DfsBinaryGroupFinderTest {
+    @Test
+    void testFindConnectedGroupsOneGroupSizeOne() {
+        int[][] image = {
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+    }
+}
