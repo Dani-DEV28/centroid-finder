e5cbc916e617b42cbed73964b1deaed601ec4ecc
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-17 18:32:26 -0700

    distance method completed, testing needed

--- src/EuclideanColorDistance.java
+++ src/EuclideanColorDistance.java
@@ -3,7 +3,8 @@ public class EuclideanColorDistance implements ColorDistanceFinder {
      * Returns the euclidean color distance between two hex RGB colors.
      * 
      * Each color is represented as a 24-bit integer in the form 0xRRGGBB, where
-     * RR is the red component, GG is the green component, and BB is the blue component,
+     * RR is the red component, GG is the green component, and BB is the blue
+     * component,
      * each ranging from 0 to 255.
      * 
      * The Euclidean color distance is calculated by treating each color as a point
@@ -19,6 +20,22 @@ public class EuclideanColorDistance implements ColorDistanceFinder {
      */
     @Override
     public double distance(int colorA, int colorB) {
-        return 0;
+        int b1 = convert(colorA, 0x0000FF, 0);
+        int b2 = convert(colorB, 0x0000FF, 0);
+
+        int g1 = convert(colorA, 0x00FF00, 8);
+        int g2 = convert(colorB, 0x00FF00, 8);
+
+        int r1 = convert(colorA, 0xFF0000, 16);
+        int r2 = convert(colorB, 0xFF0000, 16);
+
+        double dist = Math.sqrt(Math.pow(r1 - r2, 2) + Math.pow(g1 - g2, 2) + Math.pow(b1 - b2, 2));
+
+        return dist;
+    }
+
+    private int convert(int color, int mask, int shift) {
+        int preShift = color & mask;
+        return preShift >> shift;
     }
 }
