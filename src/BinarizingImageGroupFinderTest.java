919b4baf05d73238d52be612ed73ae37d0346b49
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-20 18:35:44 -0700

    test file

---
+++ src/BinarizingImageGroupFinderTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BinarizingImageGroupFinderTest {
+
+}
