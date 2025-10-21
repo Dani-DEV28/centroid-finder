070e3233a2cd7c87a99525c907846a95b942ee51
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-20 18:46:52 -0700

    tests BinarizingImageGroupFinder constructor throws correctly

--- src/BinarizingImageGroupFinderTest.java
+++ src/BinarizingImageGroupFinderTest.java
@@ -5,4 +5,23 @@ import org.junit.jupiter.api.Test;
 
 public class BinarizingImageGroupFinderTest {
 
+    @Test
+    public void testBinarizingImageGroupFinderTestNullImageBinarizer() {
+        ImageBinarizer binarizer = null;
+        BinaryGroupFinder groupFinder = new DfsBinaryGroupFinder();
+
+        assertThrows(NullPointerException.class, () -> {
+            new BinarizingImageGroupFinder(binarizer, groupFinder);
+        });
+    }
+
+    @Test
+    public void testBinarizingImageGroupFinderTestNullGroupFinder() {
+        ImageBinarizer binarizer = new DistanceImageBinarizer(null, 0, 0);
+        BinaryGroupFinder groupFinder = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            new BinarizingImageGroupFinder(binarizer, groupFinder);
+        });
+    }
 }
