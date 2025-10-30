d24a60c69a0e2649d47c3db225229cdd38e5e029
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-30 11:44:32 -0700

    fix, and dir checkCreate

--- src/main/java/com/centroidfinder/VideoProcessor.java
+++ src/main/java/com/centroidfinder/VideoProcessor.java
@@ -1,11 +1,16 @@
 package com.centroidfinder;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class VideoProcessor {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: java VideoProcessor <videoPath> <hex_target_color> <threshold>");
-            return;
-        } // can be ignore, but just checking is parameter are present
+        // if (args.length < 3) {
+        //     System.out.println("Usage: java VideoProcessor <videoPath> <hex_target_color> <threshold>");
+        //     return;
+        // } // can be ignore, but just checking is parameter are present
 
         // String videoPath = args[0];
         // String hexTargetColor = args[1];
@@ -19,6 +24,8 @@ public class VideoProcessor {
 
         String videoPath = "sampleInput/ensantina.mp4"; // path to your input video
         String outputDir = "sampleOutput/frames/"; // directory to save extracted frames
+
+        checkCreateDir(Paths.get(outputDir));
 
         String hexTargetColor = "FFA200";
         int threshold = 164; //related to try catch line 41-46
@@ -35,6 +42,8 @@ public class VideoProcessor {
         frameExt.main(videoPath, outputDir, hexTargetColor, threshold);
 
         String csvDirectory = "./sampleOutput/CSV/";
+        checkCreateDir(Paths.get(outputDir));
+
         String outputCsv = "sampleOutput/masterGroup.csv";
 
         extractCSV extractor = new extractCSV(outputCsv);
@@ -43,5 +52,18 @@ public class VideoProcessor {
         System.out.println("CSV extraction complete!");
 
         System.out.println("Frame extraction completed!");
+    }
+
+    public static void checkCreateDir(Path dirPath){
+        try {
+            if (!Files.exists(dirPath)) {
+                Files.createDirectories(dirPath); // creates parent dirs too
+                System.out.println("Directory created: " + dirPath);
+            } else {
+                System.out.println("Directory already exists: " + dirPath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
